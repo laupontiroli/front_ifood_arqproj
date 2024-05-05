d0e8e24f9fdf915cec4c4bd39fa2f92b212d2bf7
juliaas2 <juliaas@al.insper.edu.br>
2024-05-05 20:19:12 -0300

    Lista

--- app/src/Entregador/ListarEntregador.jsx
+++ app/src/Entregador/ListarEntregador.jsx
@@ -1,12 +1,18 @@
-import { Fragment, useState } from "react"
+import React, { useState, useEffect } from "react";
 import { Button, Grid, IconButton, Snackbar } from '@mui/material';
 
-
 export function ListarEntregador() {
+    const [entregadores, setEntregadores] = useState([]);
+
+
     return (
         <>
-           <h1>ListarEntregador</h1>
+           <h1>Listar Entregador</h1>
+           <ul>
+               {entregadores.map(entregador => (
+                   <li key={entregador.id}>{entregador.nome}</li>
+               ))}
+           </ul>
         </>
-    )
-
+    );
 }
